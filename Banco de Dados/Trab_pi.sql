830a8573eb647f1561e8827a647732c6cd45653b
Don616 <contatodonilo@gmail.com>
2021-09-22 08:40:47 -0300

    add pastas e arquivos da sprint

---
+++ Banco de Dados/Trab_pi.sql
@@ -0,0 +1,111 @@
+create database Busfacility;
+use busfacility;
+
+create table cadastro_empresa (
+id int primary key,
+nome varchar (50),
+representante varchar (50),
+cnpj varchar (50),
+email varchar(100),
+endereço varchar (100),
+telefone varchar (20),
+senha varchar (10)
+);
+
+
+create table info_onibus  (
+id int primary key,
+media_passageiros int,
+id_itinerario int,
+id_onibus int
+);
+
+create table registro (
+id int primary key,
+data_e_hora datetime default current_timestamp,
+contagem int,
+id_onibus int,
+id_sensor int
+);
+
+create table itinerario (
+id int primary key,
+nome_linha varchar (30),
+origem varchar (30),
+destino varchar (30)
+);
+
+create table motorista (
+id int primary key,
+nome varchar (50),
+telefone varchar (50)
+);
+
+create table onibus (
+id int primary key,
+chassi varchar (30),
+placa varchar (10),
+id_empresa int,
+id_motorista int
+);
+
+create table sensor (
+id int primary key,
+n_serie varchar (30),
+posicao varchar (30),
+id_onibus int
+);
+
+insert into cadastro_empresa
+values (1, 'Johnson', 'Neymar', '13. 222. 333/0001-44.', 'Johnson@neymar.com', 'R. Afonso silva, 138', '2641-2641', '12345667'),
+(2, 'Pfzier', 'Douglas', '22. 111. 333/0001-12.', 'pfizer@pfizer.com.br', 'R. Imparato, 23', '2645-2289', '21231214'),
+(3, 'HP', 'Harry', '33. 444. 111/0001-13.', 'HP@gmail.com', 'R. dos Alfeneiros, 138', '2246-7080', '12345678');
+
+select * from cadastro_empresa;
+
+insert into info_onibus
+values (1, 35, 1, 1),
+(2, 30, 1, 2),
+(3, 14, 2, 3),
+(4, 28, 3, 4);
+
+select * from info_onibus;
+
+insert into itinerario
+values (1, 'Jd. Bandeirantes', 'Term. Pedro II', 'R. Rua Quimanga'),
+(2, 'Jd. do Castelo', 'Term. Penha', 'Av. Cangaíba'),
+(3, 'Metro Penha', 'Av. Cangaíba', 'Term. Penha');
+
+select * from itinerario;
+
+insert into motorista
+values (1, 'Carlos', '9987-1232'),
+(2, 'Douglas', '98012-8822'),
+(3, 'Paulo', '99113-4343'),
+(4, 'Pedro', '98439-5555');
+
+select * from motorista;
+
+insert into onibus
+values (1, '9B2N3D34F', 'DER-1324', 1, 1),
+(2, 'A2D3D4G4', 'BTE-9090', 2, 2),
+(3, 'S2F3G5H6', 'SEF-1242', 3, 3),
+(4, 'G6H4J8K5', 'ZDF-1528', 4, 4);
+
+select * from onibus;
+
+insert into registro
+values (1, '2021-09-04 15:40:03', 26, 1, 1),
+(2, '2021-09-05 23:09:03', 19, 2, 2),
+(3, '2021-09-06 21:15:23', 14, 3, 3),
+(4, '2021-09-24 14:35:24', 28, 4, 4);
+
+select * from registro;
+
+insert into sensor
+values (1, '4535', 'assento da frente 3', 1),
+(2, '3545', 'corredor fundo', 2),
+(3, '7654', 'assento do fundo 1', 3),
+(4, '1345', 'assento do fundo 3', 4);
+
+select * from sensor;
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# busfacility
-Projeto de PI
+# BusFacility
+### Projeto de PI
+
+---
+
+### Sprint 02 – Semana de 18/Out
+
+#### Pesquisa e Inovação
+
+- Projetos atualizado no GitHub  /  Documentação do Projeto Atualizada
+- Planilha de Riscos do Projeto
+
+#### Algoritmos
+
+- Site Estático Institucional – Local em HTML/CSS/JavaScript
+- Site Estático Dashboard (Gráfico com ChartJS) - Local
+- Site Estático Cadastro e Login - Local
+
+#### Tecnologia da Informação
+
+- Ferramenta de Gestão de Projeto Funcionando
+- HLD do Projeto  /  LLD do Projeto
+- Planilha de BackLog /  Planilha de Sprints
+
+#### Banco de Dados
+
+- Modelagem Lógica do Projeto v1
+- Script de criação do Banco  /  Tabelas criadas em BD local
+
+#### Arquitetura Computacional
+
+- Teste com Sensor do Projeto + Gráficos
+- Diagrama de Arquitetura Local 
+- Especificação do Analytics / Métricas
