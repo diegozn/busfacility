c544827c08f6467298816c103b4e0a69d408cf69
Don616 <contatodonilo@gmail.com>
2021-10-13 09:12:27 -0300

    add alguma modificação

---
+++ Site Estruturado/html/cadastro.js
@@ -0,0 +1,14 @@
+var email;
+
+function checkEmail(){
+
+    email = input_email.value;
+    console.log(email)
+
+    if(email.indexOf('@') == -1){
+        
+    } else {
+        alert('email valido')
+    }
+
+}
