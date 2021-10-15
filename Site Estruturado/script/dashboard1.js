6848bafe7308aa62aad40b134c6e17adb47eec6b
Don616 <contatodonilo@gmail.com>
2021-10-14 22:24:43 -0300

    mvp da dashboard.css pronta

--- Site Estruturado/script/dashboard1.js
+++ Site Estruturado/script/dashboard1.js
@@ -4,25 +4,25 @@ var ctx = document.getElementById('canva1').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['7h', '8h', '9h', '10h', '11h', '12h','13h','14h'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Nº de Passageiros',
+            data: [12, 35, 19, 14, 12, 5, 30, 15],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                '#C53434',
+                '#C53434',
+                '#C53434',
+                '#C53434',
+                '#C53434',
+                '#C53434'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#C53434',
+                '#C53434',
+                '#C53434',
+                '#C53434',
+                '#C53434',
+                '#C53434'
             ],
             borderWidth: 1
         }]
--- Site Estruturado/script/dashboard2.js
+++ Site Estruturado/script/dashboard2.js
@@ -4,35 +4,23 @@ var ctx = document.getElementById('canva2').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            //label: '# of Votes',
+            data: [35, 25, 40],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                '#C0504D',
+                '#4F81BD',
+                '#9BBB59'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#C0504D',
+                '#4F81BD',
+                '#9BBB59'
             ],
             borderWidth: 1
         }]
     },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
+
 });
 
