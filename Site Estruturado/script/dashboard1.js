b1bc0716c3b4ecf94273299c41a04ac903259e80
Don616 <contatodonilo@gmail.com>
2021-10-17 19:11:05 -0300

    colocando uma lógica mais robusta na dashboard

--- Site Estruturado/script/dashboard1.js
+++ Site Estruturado/script/dashboard1.js
@@ -1,13 +1,51 @@
+// variaveis globais
+
+var myChart1;
+var myChart2;
+var frota_atual;
+var semana_atual;
+var h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,h21,h22,h23;
+filtrar.value = ``;
+
+function main(){
+
+    setData()
+    graficoBarra()
+    graficoPizza()
+
+}
+
+function limparGrafico(){
+    if(myChart1 != undefined){
+        myChart1.destroy();
+    }
+    if(myChart2 != undefined){
+        myChart2.destroy();
+    }
+
+}
 
 
+
+function filtrarFrota(){ 
+
+    limparGrafico()
+    frota_atual = filtrar.value;
+    main()
+
+}
+
+
+function graficoBarra(){
+    // Gráfico de Barra
 var ctx = document.getElementById('canva1').getContext('2d');
-var myChart = new Chart(ctx, {
+myChart1 = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['7h', '8h', '9h', '10h', '11h', '12h','13h','14h'],
+        labels: ['6h','7h', '8h', '9h', '10h', '11h', '12h','13h','14h', '15h','16h','17h','18h','19h','20h','21h','22h','23h'],
         datasets: [{
             label: 'Nº de Passageiros',
-            data: [12, 35, 19, 14, 12, 5, 30, 15],
+            data: [h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,h21,h22,h23],
             backgroundColor: [
                 '#C53434',
                 '#C53434',
@@ -35,4 +73,155 @@ var myChart = new Chart(ctx, {
         }
     }
 });
+}
+
+
+
+function graficoPizza(){
+
+var manha = (Number(h6) + Number(h7) + Number(h8) + Number(h9) + Number(h10) + Number(h11))/6
+var tarde = (Number(h12) + Number(h13) + Number(h14) + Number(h15) + Number(h16) + Number(h17))/6
+var noite = (Number(h18) + Number(h19) + Number(h20) + Number(h21) + Number(h22) + Number(h23))/6
+console.log(manha.toFixed(0), tarde.toFixed(0), noite.toFixed(0))
+
+// Gráfico de pizza
+var ctx = document.getElementById('canva2').getContext('2d');
+myChart2 = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            //label: '# of Votes',
+            data: [manha.toFixed(0), tarde.toFixed(0), noite.toFixed(0)],
+            backgroundColor: [
+                '#C0504D',
+                '#4F81BD',
+                '#9BBB59'
+            ],
+            borderColor: [
+                '#C0504D',
+                '#4F81BD',
+                '#9BBB59'
+            ],
+            borderWidth: 1
+        }]
+    },
+
+});
+}
+
+
+function setData(){
+
+    if(frota_atual == 'bandeirantes'){
+
+        h6 = (Math.random() * 10 + 25).toFixed(0);
+        h7 = (Math.random() * 10 + 25).toFixed(0);
+        h8 = (Math.random() * 10 + 25).toFixed(0);
+        h9 = (Math.random() * 10 + 25).toFixed(0);
+        h10 = (Math.random() * 10 + 25).toFixed(0);
+        h11 = (Math.random() * 10 + 25).toFixed(0);
+        h12 = (Math.random() * 10 + 25).toFixed(0);
+        h13 = (Math.random() * 10 + 25).toFixed(0);
+        h14 = (Math.random() * 10 + 25).toFixed(0);
+        h15 = (Math.random() * 10 + 25).toFixed(0);
+        h16 = (Math.random() * 10 + 25).toFixed(0);
+        h17 = (Math.random() * 10 + 25).toFixed(0);
+        h18 = (Math.random() * 10 + 25).toFixed(0);
+        h19 = (Math.random() * 10 + 25).toFixed(0);
+        h20 = (Math.random() * 10 + 25).toFixed(0);
+        h21 = (Math.random() * 10 + 25).toFixed(0);
+        h22 = (Math.random() * 10 + 25).toFixed(0);
+        h23 = (Math.random() * 10 + 25).toFixed(0);
+        
+    } else if(frota_atual == 'castelo'){
+        
+        h6 = (Math.random() * 10 + 25).toFixed(0);
+        h7 = (Math.random() * 10 + 25).toFixed(0);
+        h8 = (Math.random() * 10 + 25).toFixed(0);
+        h9 = (Math.random() * 10 + 25).toFixed(0);
+        h10 = (Math.random() * 10 + 25).toFixed(0);
+        h11 = (Math.random() * 10 + 25).toFixed(0);
+        h12 = (Math.random() * 10 + 25).toFixed(0);
+        h13 = (Math.random() * 10 + 25).toFixed(0);
+        h14 = (Math.random() * 10 + 25).toFixed(0);
+        h15 = (Math.random() * 10 + 25).toFixed(0);
+        h16 = (Math.random() * 10 + 25).toFixed(0);
+        h17 = (Math.random() * 10 + 25).toFixed(0);
+        h18 = (Math.random() * 10 + 25).toFixed(0);
+        h19 = (Math.random() * 10 + 25).toFixed(0);
+        h20 = (Math.random() * 10 + 25).toFixed(0);
+        h21 = (Math.random() * 10 + 25).toFixed(0);
+        h22 = (Math.random() * 10 + 25).toFixed(0);
+        h23 = (Math.random() * 10 + 25).toFixed(0);
+
+
+    } else if(frota_atual == 'tatuape'){
+        
+        h6 = (Math.random() * 10 + 25).toFixed(0);
+        h7 = (Math.random() * 10 + 25).toFixed(0);
+        h8 = (Math.random() * 10 + 25).toFixed(0);
+        h9 = (Math.random() * 10 + 25).toFixed(0);
+        h10 = (Math.random() * 10 + 25).toFixed(0);
+        h11 = (Math.random() * 10 + 25).toFixed(0);
+        h12 = (Math.random() * 10 + 25).toFixed(0);
+        h13 = (Math.random() * 10 + 25).toFixed(0);
+        h14 = (Math.random() * 10 + 25).toFixed(0);
+        h15 = (Math.random() * 10 + 25).toFixed(0);
+        h16 = (Math.random() * 10 + 25).toFixed(0);
+        h17 = (Math.random() * 10 + 25).toFixed(0);
+        h18 = (Math.random() * 10 + 25).toFixed(0);
+        h19 = (Math.random() * 10 + 25).toFixed(0);
+        h20 = (Math.random() * 10 + 25).toFixed(0);
+        h21 = (Math.random() * 10 + 25).toFixed(0);
+        h22 = (Math.random() * 10 + 25).toFixed(0);
+        h23 = (Math.random() * 10 + 25).toFixed(0);
+
+    } else if(frota_atual == 'morumbi'){
+        
+        h6 = (Math.random() * 10 + 25).toFixed(0);
+        h7 = (Math.random() * 10 + 25).toFixed(0);
+        h8 = (Math.random() * 10 + 25).toFixed(0);
+        h9 = (Math.random() * 10 + 25).toFixed(0);
+        h10 = (Math.random() * 10 + 25).toFixed(0);
+        h11 = (Math.random() * 10 + 25).toFixed(0);
+        h12 = (Math.random() * 10 + 25).toFixed(0);
+        h13 = (Math.random() * 10 + 25).toFixed(0);
+        h14 = (Math.random() * 10 + 25).toFixed(0);
+        h15 = (Math.random() * 10 + 25).toFixed(0);
+        h16 = (Math.random() * 10 + 25).toFixed(0);
+        h17 = (Math.random() * 10 + 25).toFixed(0);
+        h18 = (Math.random() * 10 + 25).toFixed(0);
+        h19 = (Math.random() * 10 + 25).toFixed(0);
+        h20 = (Math.random() * 10 + 25).toFixed(0);
+        h21 = (Math.random() * 10 + 25).toFixed(0);
+        h22 = (Math.random() * 10 + 25).toFixed(0);
+        h23 = (Math.random() * 10 + 25).toFixed(0);
+
+    } else {
+
+        h6 = 0
+        h7 = 0
+        h8 = 0
+        h9 = 0
+        h10 = 0
+        h11 = 0
+        h12 = 0
+        h13 = 0
+        h14 = 0
+        h15 = 0
+        h16 = 0
+        h17 = 0
+        h18 = 0
+        h19 = 0
+        h20 = 0
+        h21 = 0
+        h22 = 0
+        h23 = 0
+
+    }
+
+}
+
+
 
