a4fdc1d099483ec2a208c6d580dde4b1f430a948
Diego Leopoldino <diego.lsilva@bandtec.com.br>
2021-10-19 21:29:22 -0300

    att bug olho visivel login 1.0.2/ icone quem_somos.

--- Site Estruturado/script/login.js
+++ Site Estruturado/script/login.js
@@ -1,15 +1,15 @@
 function mostrarSenha(){
 
-    var tipo_olho = input_senha.type;
+    var tipo_olho_3 = input_senha_3.type;
     
-    if(tipo_olho == 'text'){
-        input_senha.type = 'password';
+    if(tipo_olho_3 == 'text'){
+        input_senha_3.type = 'password';
         imagem_olho.src = "../style/src/invisivel_senha.png";
-    } else if(tipo_olho == 'password'){
-        input_senha.type = 'text';
+    } else if(tipo_olho_3 == 'password'){
+        input_senha_3.type = 'text';
         imagem_olho.src = '../style/src/visivel_senha.png';
     } else{ //deixando esse else apenas para o caso do type mudar para algum outro sem querer
-        input_senha.type = 'password';
+        input_senha_3.type = 'password';
         imagem_olho.src = "../style/src/invisivel_senha.png";
     }
 }
