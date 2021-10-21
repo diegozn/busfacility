3fda8c81798766fb3fb90ccfe4668a68d2e81006
Matheus-Cantero <matheus.cantero@bandtec.com.br>
2021-10-20 22:28:12 -0300

    att site

--- Site Estruturado/script/inicial.js
+++ Site Estruturado/script/inicial.js
@@ -1,7 +1,69 @@
 function pesquisar() {
-    var pesquisa = ipt_pesquisa.value
-    var jardim = document.getElementsByClassName('jd')
-    if (pesquisa.startsWith ('jd')) {
-        jardim.style.display = 'none'
+    var pesquisa = (ipt_pesquisa.value).toLowerCase()
+    if (pesquisa.startsWith ('j') || pesquisa.startsWith ('jd') || pesquisa.startsWith ('jd.')) {
+        // aparecendo os jds
+        jd1.style.display = 'block'
+        jd2.style.display = 'block'
+        jd3.style.display = 'block'
+        jd4.style.display = 'block'
+        jd5.style.display = 'block'
+        
+        // sumindo os metros
+        metro1.style.display = 'none'
+        metro2.style.display = 'none'
+        metro3.style.display = 'none'
+        metro4.style.display = 'none'
+        metro5.style.display = 'none'
+
+        // sumindo os demais
+        ale1.style.display = 'none'
+        ale2.style.display = 'none'
+        ale3.style.display = 'none'
+        ale4.style.display = 'none'
+        ale5.style.display = 'none'
+    }
+
+    else if (pesquisa.startsWith ('m') || pesquisa.startsWith ('me') || pesquisa.startsWith ('met') || pesquisa.startsWith ('metr') || pesquisa.startsWith ('metro')) {
+        // sumindo os jds
+        jd1.style.display = 'none'
+        jd2.style.display = 'none'
+        jd3.style.display = 'none'
+        jd4.style.display = 'none'
+        jd5.style.display = 'none'
+        
+        // aparecendo os metros
+        metro1.style.display = 'block'
+        metro2.style.display = 'block'
+        metro3.style.display = 'block'
+        metro4.style.display = 'block'
+        metro5.style.display = 'block'
+
+        // sumindo os demais
+        ale1.style.display = 'none'
+        ale2.style.display = 'none'
+        ale3.style.display = 'none'
+        ale4.style.display = 'none'
+        ale5.style.display = 'none'
+    }
+
+    else {
+        // aparecendo todos
+        jd1.style.display = 'block'
+        jd2.style.display = 'block'
+        jd3.style.display = 'block'
+        jd4.style.display = 'block'
+        jd5.style.display = 'block'
+        
+        metro1.style.display = 'block'
+        metro2.style.display = 'block'
+        metro3.style.display = 'block'
+        metro4.style.display = 'block'
+        metro5.style.display = 'block'
+
+        ale1.style.display = 'block'
+        ale2.style.display = 'block'
+        ale3.style.display = 'block'
+        ale4.style.display = 'block'
+        ale5.style.display = 'block'
     }
 }
