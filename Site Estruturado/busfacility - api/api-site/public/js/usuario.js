478ff85efbb6b16f395ecbf08f79c039d5f16a68
Don616 <contatodonilo@gmail.com>
2021-11-28 23:59:19 -0300

    esqueleto da pag user quase pronta

---
+++ Site Estruturado/busfacility - api/api-site/public/js/usuario.js
@@ -0,0 +1,29 @@
+function cadastrarRotas(){
+
+    pg0.style.display = `none`;
+    pg1.style.display = `flex`;
+    pg2.style.display = `none`;
+    pg3.style.display = `none`;
+
+
+}
+
+function trocarUsuario(){
+
+    pg0.style.display = `none`;
+    pg1.style.display = `none`;
+    pg2.style.display = `flex`;
+    pg3.style.display = `none`;
+
+
+}
+
+function trocarSenha(){
+
+    pg0.style.display = `none`;
+    pg1.style.display = `none`;
+    pg2.style.display = `none`;
+    pg3.style.display = `flex`;
+
+
+}
