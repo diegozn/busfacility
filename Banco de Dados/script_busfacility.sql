47ed4db69dc512d52256f56080ab1496984c2e84
Diego <diego.lsilva@bandtec.com.br>
2021-11-30 20:49:05 -0300

    script localhost mysql

--- Banco de Dados/script_busfacility.sql
+++ Banco de Dados/script_busfacility.sql
@@ -1,4 +1,4 @@
-create database Busfacility;
+create database  Busfacility;
 use Busfacility;
 
 CREATE TABLE Empresa (
@@ -9,42 +9,51 @@ CREATE TABLE Empresa (
     email VARCHAR(100),
     endereco VARCHAR(100),
     telefoneEmpresa VARCHAR(20),
-    senha VARCHAR(10)
-)  AUTO_INCREMENT = 1;
+    senha VARCHAR(45),
+    permissaoEmpresa int
+);
 
-CREATE TABLE Itinerario (
-    idItinerario INT PRIMARY KEY AUTO_INCREMENT,
-    nomeLinha VARCHAR(30),
-    origem VARCHAR(30),
-    destino VARCHAR(30)
-) AUTO_INCREMENT = 101;
+CREATE TABLE usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nomeUsuario VARCHAR(50),
+    senhaUsuario VARCHAR(45),
+    permissaoUsuario int,
+    fkEmpresa int,
+	foreign key (fkEmpresa) references empresa(idEmpresa)
+)  AUTO_INCREMENT = 2000;
 
-CREATE TABLE Onibus (
-    idOnibus INT PRIMARY KEY AUTO_INCREMENT,
-    chassi VARCHAR(30),
-    placa VARCHAR(10),
-    fkEmpresa INT,
-    FOREIGN KEY (fkEmpresa)
-        REFERENCES Empresa (idEmpresa)
-) AUTO_INCREMENT = 201;
+create table onibus (
+idOnibus int primary key auto_increment,
+nomeLinha varchar(45),
+placa varchar(10),
+fkEmpresa int,
+foreign key (fkEmpresa) references empresa(idEmpresa)
+);
 
-CREATE TABLE Sensor (
-    idSensor INT PRIMARY KEY AUTO_INCREMENT,
-    n_Serie VARCHAR(30),
-    posicao VARCHAR(30),
-    fkOnibus INT,
-    FOREIGN KEY (fkOnibus)
-        REFERENCES Onibus (idOnibus)
-) AUTO_INCREMENT = 301;
 
-CREATE TABLE Registro (
-    idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-    dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    contagem INT,
-    fkSensor INT,
-    FOREIGN KEY (fkSensor)
-        REFERENCES Sensor (idSensor)
-) AUTO_INCREMENT = 501;
+
+create table sensor (
+idSensor int primary key auto_increment,
+nSerie varchar(45),
+posicao varchar(45),
+fkOnibus int,
+foreign key (fkOnibus) references onibus(idOnibus)
+);
+
+create table registro (
+idRegistro int primary key auto_increment,
+dataHora datetime,
+contagem int,
+fkSensor int,
+foreign key (fkSensor) references sensor(idSensor)
+);
+
+ -- insert into Registro (dataHora, contagem, fkSensor)
+-- values 
+-- ('2021-09-04 15:40:03', 26, 301),
+-- ('2021-09-05 23:09:03', 19, 302),
+-- ('2021-09-06 21:15:23', 14, 303),
+-- ('2021-09-24 14:35:24', 28, 304);
 
 insert into Empresa (nomeEmpresa, representante, cnpj, email, endereco, telefoneEmpresa, senha)
 values 
@@ -55,37 +64,31 @@ values
 
 SELECT * FROM Empresa;
 
-insert into Itinerario (nomeLinha, origem, destino)
-values 
-('Jd. Bandeirantes', 'Term. Pedro II', 'R. Rua Quimanga'),
-('Jd. do Castelo', 'Term. Penha', 'Av. Cangaíba'),
-('Metro Penha', 'Av. Cangaíba', 'Term. Penha');
 
-SELECT * FROM Itinerario;
+SELECT * FROM Onibus;
+select * from sensor;
 
-insert into Onibus (chassi, placa, fkEmpresa)
+-- insert into Sensor (nSerie, posicao, fkOnibus)
+-- values 
+-- ('4535', 'assento da frente 3', 201),
+-- ('3545', 'corredor fundo', 202),
+-- ('7654', 'assento do fundo 1', 203),
+-- ('1345', 'assento do fundo 3', 204);
+
+SELECT * FROM Sensor;
+
+
+
+insert into Onibus (nomeLinha, placa, fkEmpresa)
 values 
 ('9B2N3D34F', 'DER-1324', 1),
 ('A2D3D4G4', 'BTE-9090', 2),
 ('S2F3G5H6', 'SEF-1242', 3),
 ('G6H4J8K5', 'ZDF-1528', 4);
 
-SELECT * FROM Onibus;
-
-insert into Sensor (n_Serie, posicao, fkOnibus)
-values 
-('4535', 'assento da frente 3', 201),
-('3545', 'corredor fundo', 202),
-('7654', 'assento do fundo 1', 203),
-('1345', 'assento do fundo 3', 204);
-
-SELECT * FROM Sensor;
-
-insert into Registro (dataHora, contagem, fkSensor)
-values 
-('2021-09-04 15:40:03', 26, 301),
-('2021-09-05 23:09:03', 19, 302),
-('2021-09-06 21:15:23', 14, 303),
-('2021-09-24 14:35:24', 28, 304);
+select * from onibus;
 
 SELECT * FROM Registro;
+
+
+
